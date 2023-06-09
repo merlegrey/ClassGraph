6009832ab946d9514c80183ff112f3a018496548
partywagon112 <patrick.curtain.112@outlook.com>
2023-06-09 21:33:11 +1000

    Aesthetic changes

--- app.py
+++ app.py
@@ -1,42 +1,71 @@
-# Importing the libraries
 import dash
 from dash import html, dcc
 from dash.dependencies import Input, Output
 
 import pandas as pd
 import plotly.express as px
+import plotly.io as pio
 
-# Creating the app
+# Constants
+TITLE = "THIS NEEDS A TITLE"
+INTERVAL = 30000
+DATA_FILEPATH = "data.csv"
+
+# setup the theme
+pio.templates.default = "gridon"
+
+# Dash application instance
 app = dash.Dash(__name__)
 
-refresh_button = html.Button(
-    "Refresh", 
-    id="refresh_button", 
-    style={"color": "white", "background-color": "blue"}
-)
 
+
+
+# Callback function that creates the dataset.
 @app.callback(
-    Output("bar-graph", "figure"), # The output is the children of the output div
-    Input("refresh_button", "n_clicks") # The input is the n_clicks of the button
+    Output("bar-graph", "figure"),
+    Input("interval", "n_intervals")
 )
-def update_figure(n):
-    reading_data = pd.read_csv("data.csv")
+def create_figure(n):
+    reading_data = pd.read_csv(DATA_FILEPATH)
 
-    return px.bar(
+    # sort by highest first.
+    reading_data.sort_values("Books", inplace=True)
+
+    fig =  px.bar(
         reading_data, 
         x="Books", 
         y="Student", 
         orientation='h',
+        title=TITLE,
     )
+    # fig.update_xaxes(showticklabels=False)
+    fig.update_yaxes(visible=False)
 
+    return fig
+
+
+
+# Entry point to instanciate and run the server.
 def main():
-    fig = update_figure(0)
-
-    # Creating the layout
-    app.layout = html.Div(dcc.Graph(id="bar-graph", figure=fig))
+    # setup the layout.
+    app.layout = html.Div(
+        [
+            dcc.Graph(
+                id="bar-graph", 
+                figure=create_figure(0),
+                style={"maxHeight": "400px", "maxWidth":"700px",  "overflow": "scroll"},
+                config=dict(
+                    displayModeBar=False
+                )
+            ),
+            dcc.Interval(id="interval", interval=INTERVAL)
+        ]
+    )
 
     app.run_server(debug=False, host='0.0.0.0')
 
-# Running the app
+
+
+# run main
 if __name__ == "__main__":
     main()
